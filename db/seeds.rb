985faed776fddbf8d61fbca030943e29185cb42b
Carsten Peterson <carsten.peterson@yale.edu>
2014-12-11 16:05:28 -0500

    created seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+csv = CSV.read(File.join(Rails.root, "db", "all.csv"), :encoding => 'utf-8', :headers => true)
+
+csv.each do |my_case|
+	Case.create(date_reported: my_case[0], type_of_incident: my_case[1], date_occurred: my_case[2], time_occurred: my_case[3], location: my_case[4], disposition: my_case[5])
+end
