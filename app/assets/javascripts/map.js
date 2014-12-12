f6343205320ac62727a9a4900774e4934de288bf
Teddy Weisman <tjweisman@gmail.com>
2014-12-12 16:08:57 -0500

    added heatmap

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,18 @@
 $(".cases-map").ready(function(){
 	var mapOptions = {
-    	zoom: 13,
-    	center: new google.maps.LatLng(41.308688, -72.928701),
-    	mapTypeId: google.maps.MapTypeId.SATELLITE
+    	zoom: 14,
+    	center: new google.maps.LatLng(41.308688, -72.928701)
   	};
 
   	map = new google.maps.Map(document.getElementById('map-canvas'),
   		mapOptions);
+
+  	var points = new google.maps.MVCArray(crimeData);
+
+  	heatmap = new google.maps.visualization.HeatmapLayer({
+    data: points
+  	});
+
+  	heatmap.setMap(map);
 
 });
