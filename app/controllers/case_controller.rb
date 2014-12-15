055fe3a623530706f61ecd9dee2a1fa0608dbb31
Carsten Peterson <carsten.peterson@yale.edu>
2014-12-15 00:45:01 -0500

    got database querying from parameters working

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -1,10 +1,21 @@
 class CaseController < ApplicationController
+  include CaseHelper
+
   def search
-  	if params[:type]
-  		@cases = Case.where("type_of_incident = ?", params[:type].upcase)
-  	else
-  		@cases = Case.all
-  	end
+    puts params[:start_date]
+    start_date = Date.new(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i)
+    end_date = Date.new(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i)
+    date_range = start_date..end_date
+    start_time = DateTime.new(2000, 1, 1, params[:start_time][:hour].to_i, params[:start_time][:minute].to_i, 0)
+    end_time = DateTime.new(2000, 1, 1, params[:end_time][:hour].to_i, params[:end_time][:minute].to_i, 0)
+    time_range = start_time..end_time
+    if params[:all] != "1"
+      all_types = get_types_to_check()
+      puts params[:"DRIVING WHILE INTOXICATED-ALCOHOL"]
+      @cases = Case.where(time_occurred: time_range, date_occurred: date_range, type_of_incident: all_types)
+    else
+      @cases = Case.where(time_occurred: time_range, date_occurred: date_range)
+    end
   	render "show"
   end
   def show
