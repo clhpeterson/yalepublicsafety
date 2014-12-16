2ca7849716544b44227c631ebf119cd5ecca0890
Carsten Peterson <carsten.peterson@yale.edu>
2014-12-16 00:52:55 -0500

    fixed querying with new time and date forms

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -2,20 +2,19 @@ class CaseController < ApplicationController
   include CaseHelper
 
   def search
-    puts params[:start_date]
-    start_date = Date.new(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i)
-    end_date = Date.new(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i)
-    date_range = start_date..end_date
-    start_time = DateTime.new(2000, 1, 1, params[:start_time][:hour].to_i, params[:start_time][:minute].to_i, 0)
-    end_time = DateTime.new(2000, 1, 1, params[:end_time][:hour].to_i, params[:end_time][:minute].to_i, 0)
-    time_range = start_time..end_time
+    @start_date = Date.parse(params[:start_date])
+    @end_date = Date.parse(params[:end_date])
+    date_range = @start_date..@end_date
+    @start_hour = params[:start_time][:hour]
+    @start_minute = params[:start_time][:minute]
+    @start_ampm = params[:start_time][:ampm]
+    @start_time = Time.parse("2000-01-01 " + params[:start_time][:hour]+":"+params[:start_time][:minute]+ " " + params[:start_time][:ampm] + " -0000")
+    @end_hour = params[:end_time][:hour]
+    @end_minute = params[:end_time][:minute]
+    @end_ampm = params[:end_time][:ampm]
+    @end_time = Time.parse("2000-01-01 " + params[:end_time][:hour]+":"+params[:end_time][:minute]+ " " + params[:end_time][:ampm] + " -0000")
+    time_range = @start_time..@end_time
     @cases = Case.where(time_occurred: time_range, date_occurred: date_range, type_of_incident: params[:incidents], weekday: params[:days])
-
-
-    @start_time = Time.new(2000, 1, 1, params[:start_time][:hour].to_i, params[:start_time][:minute].to_i, 0)
-    @end_time = Time.new(2000, 1, 1, params[:end_time][:hour].to_i, params[:end_time][:minute].to_i, 0)
-    @start_date = Date.new((params[:start_date][:year]).to_i, (params[:start_date][:month]).to_i, (params[:start_date][:day]).to_i)
-    @end_date = Date.new((params[:end_date][:year]).to_i, (params[:end_date][:month]).to_i, (params[:end_date][:day]).to_i)
 
     if params[:days] != nil
       @days = params[:days]
@@ -38,8 +37,12 @@ class CaseController < ApplicationController
     @days = Case::DAYS
     @start_date = Date.new(2013, 5, 17)
     @end_date = Date.today
-    @start_time = Time.new(2000, 1, 1, 0, 0, 0)
-    @end_time = Time.new(2000, 1, 1, 23, 59, 0)
+    @start_hour = "12"
+    @start_minute = "00"
+    @start_ampm = "AM"
+    @end_hour = "11"
+    @end_minute = "59"
+    @end_ampm = "PM"
     @incidents = []
     @categories = Array.new
   	@cases = Case.all
