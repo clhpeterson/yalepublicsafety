8711bebb8662179cf0d952e777a1f0579ff99d5d
Carsten Peterson <carsten.peterson@yale.edu>
2014-12-15 22:27:45 -0500

    added weekday checkboxes

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -9,18 +9,19 @@ class CaseController < ApplicationController
     start_time = DateTime.new(2000, 1, 1, params[:start_time][:hour].to_i, params[:start_time][:minute].to_i, 0)
     end_time = DateTime.new(2000, 1, 1, params[:end_time][:hour].to_i, params[:end_time][:minute].to_i, 0)
     time_range = start_time..end_time
-    if params[:all] != "1"
-      all_types = get_types_to_check()
-      puts params[:"DRIVING WHILE INTOXICATED-ALCOHOL"]
-      @cases = Case.where(time_occurred: time_range, date_occurred: date_range, weekday: params[:weekday][:day], type_of_incident: all_types)
-    else
-      @cases = Case.where(time_occurred: time_range, date_occurred: date_range, weekday: params[:weekday][:day])
-    end
+    @cases = Case.where(time_occurred: time_range, date_occurred: date_range, type_of_incident: params[:incidents], weekday: params[:days])
+
+
     @start_time = Time.new(2000, 1, 1, params[:start_time][:hour].to_i, params[:start_time][:minute].to_i, 0)
     @end_time = Time.new(2000, 1, 1, params[:end_time][:hour].to_i, params[:end_time][:minute].to_i, 0)
     @start_date = Date.new((params[:start_date][:year]).to_i, (params[:start_date][:month]).to_i, (params[:start_date][:day]).to_i)
     @end_date = Date.new((params[:end_date][:year]).to_i, (params[:end_date][:month]).to_i, (params[:end_date][:day]).to_i)
-    @all = params[:all]
+
+    if params[:days] != nil
+      @days = params[:days]
+    else
+      @days = Array.new
+    end
     if params[:incidents] != nil
       @incidents = params[:incidents]
     else
@@ -28,15 +29,13 @@ class CaseController < ApplicationController
     end
     if params[:categories] != nil
       @categories = params[:categories]
-      puts 'sldkflsdkjf'
-      puts params[:categories]
     else
       @categories = Array.new
     end
-    puts params[:categories]
   	render "show"
   end
   def show
+    @days = Case::DAYS
     @start_date = Date.new(2013, 5, 17)
     @end_date = Date.today
     @start_time = Time.new(2000, 1, 1, 0, 0, 0)
